bf096df342594d11b8e74200f20f0856c22fa861
Adrian Pop <adrian.pop@liu.se>
2022-03-18 15:23:17 +0100

    set gradle user home

--- project-run.bat
+++ project-run.bat
@@ -1,6 +1,6 @@
 REM run this to download the files and compile everything
 @echo on
 
+set GRADLE_USER_HOME=.\gradle-dependencies
 call gradlew.bat translate --args="tests/unit/" --warning-mode all
-
 pause
