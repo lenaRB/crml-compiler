f168b5a5a668da6c67d5644008a8f437ed3c1061
Lena Buffoni <olero90@lena.liu.se>
2022-11-23 21:26:07 +0100

    restructure to use junit tests

---
+++ src/tests/tests/basicTypeTests.java
@@ -0,0 +1,20 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class basicTypeTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}	
+	
+	@Test
+	void test2() {
+		fail("Not yet implemented");
+	}	
+
+
+}
