f3fab41281a997d37696c8c58b7d51ad2b5cfddf
Adrian Pop <adrian.pop@liu.se>
2022-11-28 12:54:49 +0100

    fix windows testing

--- src/test/java/useCaseTests/useCaseTest.java
+++ src/test/java/useCaseTests/useCaseTest.java
@@ -1,6 +1,8 @@
 package useCaseTests;
 
 import static org.junit.jupiter.api.Assertions.fail;
+import java.io.File;
+
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -13,43 +15,44 @@ import crml.translator.Main;
 import nl.altindag.log.LogCaptor;
 
 public class useCaseTest {
-	
-	 static LogCaptor logCaptor;
-	 static String testFolder,  outputFolder;	 
-	 
-	 @BeforeAll
-	    public static void setupLogCaptor() {
-	        logCaptor = LogCaptor.forClass(Main.class);
-	        
-	        testFolder = Thread.currentThread().getContextClassLoader().getResource("testFiles" + java.io.File.separator +"use-cases").getPath();  
-	    }	
+  
+   static LogCaptor logCaptor;
+   static String testFolder,  outputFolder;   
+   
+   @BeforeAll
+      public static void setupLogCaptor() {
+          logCaptor = LogCaptor.forClass(Main.class);
+          
+          testFolder = Thread.currentThread().getContextClassLoader().getResource("testFiles").getPath();
+          testFolder = new File(testFolder).getAbsolutePath() + java.io.File.separator + "use-cases";
+      }  
 
 
-	    @AfterEach
-	    public void clearLogs() {
-	        logCaptor.clearLogs();
-	    }
-	    
-	    @AfterAll
-	    public static void tearDown() {
-	        logCaptor.close();
-	    } 
+      @AfterEach
+      public void clearLogs() {
+          logCaptor.clearLogs();
+      }
+      
+      @AfterAll
+      public static void tearDown() {
+          logCaptor.close();
+      } 
 
-	    @DisplayName("Testing use-cases")
-	    @ParameterizedTest
-	    @ValueSource(strings = {"requirements_tank.crml", 
-	    		"TwoTanks.crml", 
-	    		"Contract.crml"})
-	    void testPrimitiveTypes(String fileName) {
-	    	try {
-	    		
-				crml.translator.Main.parse_file(testFolder, fileName, "tmpCRML", true);
-				
-			} catch (Exception e) {
-				fail("Unable to translate " + fileName + ":\n" + e.getMessage());
-			}
-	    	
-	}	
-	
+      @DisplayName("Testing use-cases")
+      @ParameterizedTest
+      @ValueSource(strings = {"requirements_tank.crml", 
+          "TwoTanks.crml", 
+          "Contract.crml"})
+      void testPrimitiveTypes(String fileName) {
+        try {
+          
+        crml.translator.Main.parse_file(testFolder, fileName, "tmpCRML", true);
+        
+      } catch (Exception e) {
+        fail("Unable to translate " + fileName + ":\n" + e.getMessage());
+      }
+        
+  }  
+  
 
 }
