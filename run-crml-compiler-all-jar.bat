446871bd5383392f90c39cdb4368722c81741c0f
Adrian Pop <adrian.pop@liu.se>
2023-12-07 11:14:15 +0100

    pass all the parameters to the compiler

--- run-crml-compiler-all-jar.bat
+++ run-crml-compiler-all-jar.bat
@@ -1,4 +1,5 @@
 
 REM usage: .\run-crml-compiler-all.bat .\src\test\resources\testModels\libraries\ETL_test\DecideOver.crml
 
-java -jar crml-compiler-all.jar %1 %2 %3 %4 %5 %6 %7 %8
+java -jar crml-compiler-all.jar %*
+
